76a383f7096f56fba5552749b02fe529214f4eef
180204106417 <1274827341@qq.com>
2021-01-12 20:28:57 +0800

    commit four

--- student.java
+++ student.java
@@ -1,6 +1,9 @@
-import java.util.*
-class student{
-  private string name;
-  private string address;
-  private string email;   //修改的内容
+Student.java
+	import java.util.*
+	class student{
+		private string name;
+		private string address;
+		private string email;
+		private String telephone;   //新增电话属性
 }
+
